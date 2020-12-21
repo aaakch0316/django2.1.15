1a0912cd6917e0326f06d143f105d52fda41248b
강창현 <aaakch0316@gmail.com>
2020-12-21 23:09:13 +0900

    django3 [+] new.html

--- django/articles/views.py
+++ django/articles/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 def index(request):
+    articles = Article.objects.all()
+    content = {
+        'articles' : articles
+    }
     return render(request, 'articles/index.html')
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
