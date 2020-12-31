dea690e002bd6ad53ebbca8bb3d5924f12a24adf
강창현 <aaakch0316@gmail.com>
2020-12-31 11:34:27 +0900

    polls [+] init

--- django/articles/urls.py
+++ django/articles/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('new/', views.new),
     path('create/', views.create),
     path('<int:pk>/', views.detail),
     path('<int:pk>/delete/', views.delete),
--- django/articles/views.py
+++ django/articles/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Article
 
+from .forms import ArticleForm
+
+
 # Create your views here.
 def index(request):
     articles = Article.objects.all()
@@ -9,15 +12,30 @@ def index(request):
     }
     return render(request, 'articles/index.html', context)
 
-def new(request):
-    return render(request, 'articles/new.html')
+# def new(request):
+#     return render(request, 'articles/new.html')
+
+# def create(request):
+#     title = request.GET.get('title')
+#     content = request.GET.get('content')
+#     article = Article(title=title, content=content)
+#     article.save()
+#     return redirect('/articles/')
 
 def create(request):
-    title = request.GET.get('title')
-    content = request.GET.get('content')
-    article = Article(title=title, content=content)
-    article.save()
-    return redirect('/articles/')
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save()
+            return redirect('/articles/')
+    else:
+        form = ArticleForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'articles/new.html', context)
+
+
 
 def detail(request, pk):
     article = Article.objects.get(pk=pk)
