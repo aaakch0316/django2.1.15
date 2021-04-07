1b255b180937aa3f7b9ae58a9905f9111d01a1d6
강창현 <aaakch0316@gmail.com>
2021-04-07 14:45:53 +0900

    test

--- instagram/accounts/forms.py
+++ instagram/accounts/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserChangeForm, UserChangeForm
+from django.contrib.auth.forms import UserChangeForm
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
