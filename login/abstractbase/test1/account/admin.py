db04b642da40fd5aeeff6eab7a0e0f033a5f93a2
강창현 <aaakch0316@gmail.com>
2021-04-09 15:53:20 +0900

    custom user model [+] init

--- login/abstractbase/test1/account/admin.py
+++ login/abstractbase/test1/account/admin.py
@@ -1,4 +1,13 @@
 # from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+# from .models import MyUser
+
+# admin.site.register(MyUser, UserAdmin)
+
+
+
+
+# from django.contrib import admin
 
 # # Register your models here.
 # from django import forms
